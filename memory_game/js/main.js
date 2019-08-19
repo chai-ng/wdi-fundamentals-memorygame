c41f4c6a0eedacb71aeb9ba434a65124b6325313
Chai Ng <chaing@Chais-MacBook-Pro.local>
2019-08-19 13:07:32 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,17 @@
-console.log("Up and running!");
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
 
-var cardOne = 'Queen';
-var cardTwo = 'King';
-var cardThree = 'Queen';
-var cardFour = 'King';
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardsInPlay);
 
-console.log("User flipped " + cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardsInPlay);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	}
+	alert('Sorry, try again.')
+}
